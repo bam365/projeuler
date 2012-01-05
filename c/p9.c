74283bf9f9665577e0119025bda976b748123954
bam365 <bam365@mst.edu>
2012-01-05 18:27:12 +0000

    Problem 9

---
+++ c/p9.c
@@ -0,0 +1,29 @@
+/* p9.c - Project Euler problem 9, Find the first Pythongorean triplet where
+ *        a+b+c=1000
+ * Blake Mitchell, 2012
+ */
+
+#include <stdio.h>
+
+#define SUM 1000
+#define MAX 1000
+
+main()
+{
+        int a, b, c;
+        int found = 0;
+        
+        /* Mmmm, brute force */
+        for (a = 1; a <= MAX && !found; a++) {
+                for (b = a; b <= MAX && !found; b++) {
+                        for (c = b; c <= MAX && !found; c++) {
+                                if (a + b + c == SUM &&
+                                    a*a + b*b == c*c)
+                                        found = 1;
+                        }
+                }
+        }
+
+        printf("A pythagorean triplet where a+b+c=1000: \n");
+        printf("a = %d, b = %d, c = %d\n", a, b, c);
+}
