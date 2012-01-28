5764b9d484f5eb0f9ce0acf3f53e7384ca5364d3
bam365 <bam365@mst.edu>
2012-01-28 01:25:46 -0600

    Initial commit

---
+++ go/p28.go
@@ -0,0 +1,53 @@
+//p28.go - Project Euler problem 28
+//Blake Mitchell, 2012
+
+package main;
+
+import (
+        "fmt"
+        "big"
+)
+
+
+const SPIRAL_RADIUS = 501 
+const SPIRAL_DIAMETER = SPIRAL_RADIUS*2 - 1
+
+
+
+func main() {
+        leftdiag, rightdiag := big.NewInt(1), big.NewInt(0)
+        lc, sl := big.NewInt(0), big.NewInt(0)
+        dsum := big.NewInt(0)
+        for i := int64(2); i <= SPIRAL_RADIUS; i++ {
+                lc = spiral_upper_left_corner(i)
+                sl.Sub(spiral_side_len(i), big.NewInt(1))
+                rightdiag.Add(rightdiag, lc)
+                lc.Sub(lc, sl)
+                leftdiag.Add(leftdiag, lc)
+                lc.Sub(lc, sl)
+                rightdiag.Add(rightdiag, lc)
+                lc.Sub(lc, sl)
+                leftdiag.Add(leftdiag, lc)
+        }
+        dsum.Add(leftdiag, rightdiag)
+        fmt.Printf("Sum of diagonals in %dx%d spiral: ", SPIRAL_DIAMETER,
+                                                         SPIRAL_DIAMETER);
+        fmt.Println(dsum)
+}
+
+
+func spiral_upper_left_corner(iter int64) *big.Int {
+        n := spiral_side_len(iter)
+        n.Mul(n, n)
+        return n
+}
+
+
+func spiral_side_len(iter int64) *big.Int {
+        //The big package is incredibly awkward, so in case you can't tell,
+        // side_len = iter*2 - 1
+        n := big.NewInt(0)        
+        n = n.Add(n.Mul(big.NewInt(iter), big.NewInt(2)), big.NewInt(-1))
+        return n 
+}
+
