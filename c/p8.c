878c89e5a5d0bbde4d6d42b8e9d8ac8eb7cd8859
bam365 <bam365@mst.edu>
2012-05-25 09:06:20 +0000

    Added problem 8

--- c/p8.c
+++ c/p8.c
@@ -4,14 +4,31 @@
  */
 
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_INPUT_SIZE 4096 
+#define SECSIZE 5
+#define DIGTONUM(x) ((int)(x - '0'))
 
 
 int read_number(FILE *f, char *buf, int maxsize);
 
 
+main() 
+{
+        char *number;
+        int maxprod;
+        
+        number = (char*)malloc(MAX_INPUT_SIZE);
+        memset(number, '\0', MAX_INPUT_SIZE);
+
+        read_number(stdin, number, MAX_INPUT_SIZE);
+        maxprod = max_digit_prod(number, strlen(number), SECSIZE);
+        printf("The maximum product of %d digits in the 1000-digit number "
+               " is %d\n", SECSIZE, maxprod);
+}
+        
 
 
 
@@ -28,5 +45,21 @@ int read_number(FILE *f, char *buf, int maxsize)
 }
                 
 
-int max_digit_sum(char *buf
+int max_digit_prod(char *buf, int size, int secsize) 
+{
+        int i, prod, maxprod = 1;
+        char *d;
+
+        for (d = buf; (d - buf) < (size - secsize); d++) {
+                for (i = 0, prod = 1; i < secsize; i++)
+                        prod *= DIGTONUM(d[i]);
+                if (prod > maxprod)
+                        maxprod = prod;
+        }
+
+        return maxprod;
+}
+
+
+
 
